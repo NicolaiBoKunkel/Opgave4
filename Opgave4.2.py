be8f771a5bda3d07e8dc92d8b69571707b95eec7
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2023-10-02 16:28:11 +0200

    done

---
+++ Opgave4.2.py
@@ -0,0 +1,43 @@
+#Server
+
+from socket import *
+import threading
+import random
+
+def handleOneClient(connectionSocket, address):
+    print('Address', address)
+
+    data = connectionSocket.recv(2048).decode()
+    parts = data.split(';')
+
+    if len(parts) == 3:
+        operation = parts[0]
+        num1 = int(parts[1])
+        num2 = int(parts[2])
+
+        if operation == 'Add':
+            result = num1 + num2
+        elif operation == 'Subtract':
+            result = num1 - num2
+        elif operation == 'Random':
+            result = random.randint(num1, num2)
+        else:
+            result = 'Invalid input'
+
+        response = f'Result;{result}'
+        connectionSocket.send(response.encode())
+    else:
+        connectionSocket.send('Invalid input'.encode())
+
+    connectionSocket.close()
+
+serverPort = 10
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print('Server is ready to work for you')
+
+while True:
+    clientSocket, clientAddress = serverSocket.accept()
+    threading.Thread(target=handleOneClient, args=(clientSocket, clientAddress)).start()
---
+++ Opgave4.py
@@ -0,0 +1,22 @@
+#Klient Opgave 4
+
+from socket import *
+
+servername = "localhost"
+serverport = 10
+
+clientsocket = socket(AF_INET, SOCK_STREAM)
+clientsocket.connect((servername, serverport))
+
+operation = input("indtast funktion (Add, Random eller Subtract):")
+num1 = input('Indtast tal 1:')
+num2 = input('Indtast tal 2:')
+data = (operation+';'+num1+';'+num2).encode()
+clientsocket.send(data)
+
+datatilbage = clientsocket.recv(2048)
+sentanceTilbage = datatilbage.decode()
+
+print ("modtaget resultat", sentanceTilbage)
+
+clientsocket.close
